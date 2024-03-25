a6e316f417c25be6366c305d268e141f351a82ab
kylercondran@gmail.com <kylercondran@gmail.com>
2024-03-25 20:07:05 -0300

    added a medium solution

--- LeetCode/Medium.cs
+++ LeetCode/Medium.cs
@@ -2218,6 +2218,36 @@ namespace LeetCode
             }
             return ans;
         }
+        //Title: 148. Sort List
+        //Link: https://leetcode.com/problems/sort-list
+        //Tags: Linked List, Two Pointers, Divide and Conquer, Sorting, Merge Sort
+        public static ListNode SortList(ListNode head)
+        {
+            List<int> a = new List<int>();
+            ListNode ans = null;
+            while (head != null)
+            {
+                a.Add(head.val);
+                head = head.next;
+            }
+            a.Sort();
+            ListNode current = ans;
+            foreach (int i in a)
+            {
+                ListNode link = new ListNode(i);
+                if (ans == null)
+                {
+                    ans = link;
+                    current = ans;
+                }
+                else
+                {
+                    current.next = link;
+                    current = current.next;
+                }
+            }
+            return ans;
+        }
         #endregion
     }
 }
