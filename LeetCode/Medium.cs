61ccc026577b7b8c04e7b0a3675155a13da4504f
kylercondran@gmail.com <kylercondran@gmail.com>
2024-03-28 23:24:31 -0300

    added a medium solution

--- LeetCode/Medium.cs
+++ LeetCode/Medium.cs
@@ -2722,6 +2722,20 @@ namespace LeetCode
             }
             return ans;
         }
+        //Title: 53. Maximum Subarray
+        //Link: https://leetcode.com/problems/maximum-subarray
+        //Tags: Array, Divide and Conquer, Dynamic Programming
+        public static int MaxSubArray(int[] nums)
+        {
+            int sum = nums[0];
+            int max = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                sum = Math.Max(nums[i], sum + nums[i]);
+                max = Math.Max(max, sum);
+            }
+            return max;
+        }
         #endregion
     }
 }
