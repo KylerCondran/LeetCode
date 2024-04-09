74f03311a7fb9ed731f43e24f2ec51a984af21c3
kylercondran@gmail.com <kylercondran@gmail.com>
2024-04-09 17:28:33 -0300

    added a medium design solution

--- LeetCode/Design.cs
+++ LeetCode/Design.cs
@@ -830,5 +830,53 @@ namespace LeetCode
             return a[rand];
         }
     }
+    //Title: 146. LRU Cache
+    //Link: https://leetcode.com/problems/lru-cache
+    //Difficulty: Medium
+    //Tags: Hash Table, Linked List, Design, Doubly-Linked List
+    public class LRUCache
+    {
+        Dictionary<int, int> a;
+        List<int> b;
+        int cap;
+        public LRUCache(int capacity)
+        {
+            this.a = new Dictionary<int, int>();
+            this.b = new List<int>();
+            this.cap = capacity;
+        }
+        public int Get(int key)
+        {
+            if (a.ContainsKey(key))
+            {
+                b.Remove(key);
+                b.Add(key);
+                return a[key];
+            }
+            else
+            {
+                return -1;
+            }
+        }
+        public void Put(int key, int value)
+        {
+            if (!a.ContainsKey(key))
+            {
+                if (a.Count == cap)
+                {
+                    a.Remove(b[0]);
+                    b.Remove(b[0]);
+                }
+                b.Add(key);
+                a.Add(key, value);
+            }
+            else
+            {
+                b.Remove(key);
+                b.Add(key);
+                a[key] = value;
+            }
+        }
+    }
     #endregion
 }
