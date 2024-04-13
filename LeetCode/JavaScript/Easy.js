fd002120667743a41ecb4865cd94da067f5701db
kylercondran@gmail.com <kylercondran@gmail.com>
2024-04-13 15:19:58 -0300

    added 4 javascript easy solutions

---
+++ LeetCode/JavaScript/Easy.js
@@ -0,0 +1,49 @@
+//Title: 2703. Return Length of Arguments Passed
+//Link: https://leetcode.com/problems/return-length-of-arguments-passed
+//Tags: JavaScript
+var argumentsLength = function (...args) {
+    x = 0;
+    for (let variable in args) {
+        x += 1;
+    }
+    return x;
+};
+//Title: 2667. Create Hello World Function
+//Link: https://leetcode.com/problems/create-hello-world-function
+//Tags: JavaScript
+var createHelloWorld = function () {
+    return function (...args) {
+        return HelloWorld();
+    }
+};
+function HelloWorld() {
+    return 'Hello World';
+};
+//Title: 2620. Counter
+//Link: https://leetcode.com/problems/counter
+//Tags: JavaScript
+var createCounter = function (n) {
+
+    return function () {
+        return n++;
+    };
+};
+//Title: 2665. Counter II
+//Link: https://leetcode.com/problems/counter-ii
+//Tags: JavaScript
+var createCounter = function (init) {
+    let val = init;
+    function increment() {
+        val++;
+        return val;
+    };
+    function decrement() {
+        val--;
+        return val;
+    };
+    function reset() {
+        val = init;
+        return val;
+    };
+    return { increment, decrement, reset };
+};
