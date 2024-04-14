280d863536c04559705dab1b79f5bc23816042f0
kylercondran@gmail.com <kylercondran@gmail.com>
2024-04-13 23:57:39 -0300

    added an easy python solution, modified tags

--- LeetCode/Pandas/Easy.py
+++ LeetCode/Pandas/Easy.py
@@ -1,7 +1,14 @@
 import pandas as pd
 #Title: 2879. Display the First Three Rows
 #Link: https://leetcode.com/problems/display-the-first-three-rows
-#Tags: Python, Database
+#Tags: Database
 def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
     df = pd.DataFrame(employees)
     return df.head(3)
+#Title: 2881. Create a New Column
+#Link: https://leetcode.com/problems/create-a-new-column
+#Tags: Database
+def createBonusColumn(employees: pd.DataFrame) -> pd.DataFrame:
+    df = pd.DataFrame(employees)
+    df['bonus'] = df.apply(lambda column: column.salary * 2, axis=1)
+    return df
