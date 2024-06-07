a52ac10795370b642652f46c4dd74bb20c172ad3
kylercondran@gmail.com <kylercondran@gmail.com>
2024-06-07 12:01:28 -0300

    added a medium solution

--- LeetCode/Medium.cs
+++ LeetCode/Medium.cs
@@ -5460,6 +5460,27 @@ namespace LeetCode
             }
             return ans;
         }
+        //Title: 2491. Divide Players Into Teams of Equal Skill
+        //Link: https://leetcode.com/problems/divide-players-into-teams-of-equal-skill
+        //Tags: Array, Hash Table, Two Pointers, Sorting
+        public static long DividePlayers(int[] skill)
+        {
+            List<int> a = new List<int>();
+            for (int i = 0; i < skill.Length; i++) a.Add(skill[i]);
+            a.Sort();
+            long product = 0;
+            int score = a.Min() + a.Max();
+            while (a.Count > 0)
+            {
+                int min = a.Min();
+                int max = a.Max();
+                if (min + max != score) return -1;
+                product += min * max;
+                a.RemoveAt(0);
+                a.RemoveAt(a.Count - 1);
+            }
+            return product;
+        }
     }
     #endregion
     #region "Medium Classes"
